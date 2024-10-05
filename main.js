f538dac266942fddff9ff6478d820f48d41c1bf8
Ch-Hina <chhinamustafa07@gmail.com>
2024-10-05 05:00:49 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+ //var data = true;
+
+//  var type = typeof(data);
+
+
+// console.log(type);
+
+//if i am wriiten true or false then terminl show boolean 
+//i am wriiten "Hello world"then terminl show string 
+//i am wriiten number then terminl show number  
+
+// var data = null;
+
+// var type = typeof(data);
+
+
+// console.log(type);
+// null mean memory block are create but data are not assign (mean memory  block are empty)
+
+var data = {name : "Shanza" , age : 20 };
+
+var type = typeof(data);
+
+
+console.log(type);
+console.log(data)
+
+
+
+function sum(a,b)  {
+    return a+b;
+}
+
+
+
+var num1 = 1;
+var num2 = 10;
+
+var sum1 = sum(num1 , num2);
+var sum2= sum (30 , 40);
+
+console.log(sum1)
+console.log (sum2)
+
+
+
+
