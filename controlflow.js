60eef20d3b9119821ea839351297a88eff3e9f2b
Ch-Hina <chhinamustafa07@gmail.com>
2024-10-05 06:44:09 -0700

    if else statement

---
+++ controlflow.js
@@ -0,0 +1,48 @@
+
+
+//conditional statement
+
+// var  age = 12
+//   if ( age  >= 22) {
+//    console.log("Yes is adult ")
+// }  
+//    else if ( age >= 21) {
+
+//     console.log ("You will be next year")
+
+//    }
+//    else if (age <= 13) {
+    
+//     console.log("Your are a teenagra ")
+
+//    }
+    
+
+// else{
+
+//     console.log("NO,this is not")
+
+// }
+
+
+//Switch statewment 
+
+var age = 13
+
+switch (age) {
+
+    case age >= 22:
+    console.log(" Your are adult");s
+    break;
+
+    case age == 21 :
+    console.log(" try next year");
+    break;
+
+    case  age <= 13:
+    console.log("your are teenagra");
+    break;
+
+    default:
+        console.log("NO!, You are not")
+}
